26f15eee896b956ccb9fcfc4fc03892bd2a65c19
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2024-02-06 01:10:54 +0530

    Notification see and search

--- src/app/main/notification/page.jsx
+++ src/app/main/notification/page.jsx
@@ -1,12 +1,72 @@
 "use client";
-import { Box, Button, Typography } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useMemo, useState } from "react";
 import SendNotification from "./SendNotification";
+import { useQuery } from "@tanstack/react-query";
+import { getNotifications } from "@/services/notification.service";
+import { DataGrid } from "@mui/x-data-grid";
+import { enqueueSnackbar } from "notistack";
+import { controllerToken } from "@/config/temp.config";
+import { Search } from "@mui/icons-material";
+import { format } from "date-fns";
 
 const Notification = () => {
   const [sendNotification, setSendNotification] = useState({
     open: false,
   });
+  const [search, setSearch] = useState("");
+
+  const notificationRes = useQuery({
+    queryKey: ["notifications"],
+    queryFn: () => getNotifications(controllerToken),
+    retry: 2,
+    staleTime: 1000,
+    gcTime: 1000 * 2,
+  });
+
+  if (notificationRes.isError) {
+    enqueueSnackbar({
+      variant: "error",
+      message:
+        notificationRes.error.response.status +
+        " : " +
+        notificationRes.error.response.data.message,
+    });
+  }
+
+  const cols = [
+    { field: "title", headerName: "Title", flex: 1 },
+    { field: "message", headerName: "Message", flex: 1 },
+    {
+      field: "createdAt",
+      headerName: "Sent At",
+      flex: 0.5,
+      renderCell: (params) => {
+        return format(new Date(params.value), "do MMM yyyy");
+      },
+    },
+  ];
+
+  const rows = useMemo(() => {
+    return notificationRes.data?.data?.notifications
+      .map((ele, idx) => ({
+        ...ele,
+        id: idx + 1,
+      }))
+      .filter((row) => {
+        return (
+          row.title.toLowerCase().includes(search.toLowerCase()) ||
+          row.message.toLowerCase().includes(search.toLowerCase())
+        );
+      });
+  }, [search, notificationRes.data?.data?.notifications]);
 
   return (
     <Box>
@@ -14,6 +74,7 @@ const Notification = () => {
         sx={{
           display: "flex",
           justifyContent: "space-between",
+          mb: 3,
         }}
       >
         <Box>
@@ -37,6 +98,48 @@ const Notification = () => {
           />
         </Box>
       </Box>
+      {notificationRes.isLoading ? (
+        <Box>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "end",
+            }}
+          >
+            <TextField
+              placeholder="Search Notifications"
+              variant="standard"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Search />
+                  </InputAdornment>
+                ),
+              }}
+              sx={{ mb: 1, minWidth: 300 }}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </Box>
+          <Box>
+            {console.log(notificationRes.data?.data?.notifications)}
+            <DataGrid
+              rows={rows}
+              columns={cols}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              disableSelectionOnClick
+              disableRowSelectionOnClick
+              disableColumnSelector
+              disableColumnFilter
+            />
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 };
--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -1,6 +1,24 @@
 import { BaseUrl } from "@/config/var.config";
 import axios from "axios";
 
+export const getNotifications = async (token) => {
+  try {
+    const response = await axios.get(
+      `${BaseUrl}/exam-controller/get-notifications`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
 export const sendNotification = async (notification, token) => {
   try {
     const response = await axios.post(
