b3eaddebd5a61a2e94accccdd02f25ca95dd43d1
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2024-03-07 16:56:00 +0530

    Change backend

--- src/config/var.config.ts
+++ src/config/var.config.ts
@@ -1,3 +1,3 @@
-export const BaseUrl = "http://localhost:3000";
-// export const BaseUrl = "https://backend.parikshamitr.tech";
+// export const BaseUrl = "http://localhost:3000";
+export const BaseUrl = "https://backend.parikshamitr.tech";
 // export const BaseUrl = "http://144.24.135.11:3000";
