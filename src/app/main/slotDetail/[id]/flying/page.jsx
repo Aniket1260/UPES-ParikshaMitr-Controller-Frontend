d07e7bef7da9373a63d36109211f40529ffe6bed
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2024-03-23 02:38:26 +0530

    Added a flying page

---
+++ src/app/main/slotDetail/[id]/flying/page.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const SlotFlying = () => {
+  return (
+    <Box>
+      <Typography variant="h4">Flying Details</Typography>
+    </Box>
+  );
+};
+
+export default SlotFlying;
